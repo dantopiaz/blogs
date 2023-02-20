1a8babbcdf3bcd2464aa5de4ce0e342058d5f6a9
Dan Tong <tongvtdan@outlook.com>
2023-02-20 15:42:35 +0700

    Update files

--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -7,8 +7,8 @@ var documents = [{
     }, {
     "id": 1,
     "url": "http://localhost:4000/about",
-    "title": "Mediumish Template for Jekyll",
-    "body": "This website is built with Jekyll and Mediumish template for Jekyll. It's for demonstration purposes, no real content can be found. Mediumish template for Jekyll is compatible with Github pages, in fact even this demo is created with Github Pages and hosted with Github.  Documentation: Please, read the docs here. Questions or bug reports?: Head over to our Github repository! Buy me a coffeeThank you for your support! Your donation helps me to maintain and improve Mediumish . Buy me a coffee Documentation"
+    "title": "About me",
+    "body": "This blog website is a space we share and learn all about technology, innovation and more. Support usThank you for your support! "
     }, {
     "id": 2,
     "url": "http://localhost:4000/categories",
