ed0f57a8cda3358d36baa4455368f51afb832297
Dan Tong <tongvtdan@outlook.com>
2023-02-23 16:12:07 +0700

    Cập nhật bài đăng "Chào mừng đến với dantopia"

--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -13,7 +13,7 @@ var documents = [{
     "id": 2,
     "url": "http://localhost:4000/author-dantino.html",
     "title": "Dantino",
-    "body": "                        Dantino Follow:                                                    Posts by Dantino:                   		Welcome to dantopia. vn	: 		  Welcome to dantopia. vn and its contents those are delivered to you on its blogs. 	 			Trong 				dantopia, 								Feb 20, 2023						        "
+    "body": "                        Dantino Follow:                                                    Posts by Dantino:                   		Chào mừng đến với trang blog của dantopia. vn	: 		  Lời chàoXin chào, tôi là Tống Vũ Thân Dân, dantopia. vn là trang web cá nhân và trang web này là nơi tôi chia sẻ các kinh nghiệm, kiếm thức,… xoay quanh các chủ đề:	 			Trong 				dantopia, 								Feb 20, 2023						        "
     }, {
     "id": 3,
     "url": "http://localhost:4000/categories",
@@ -23,7 +23,7 @@ var documents = [{
     "id": 4,
     "url": "http://localhost:4000/",
     "title": "Home",
-    "body": "      Featured:                                                                                                                                                                                                           Welcome to dantopia. vn                              :               Welcome to dantopia. vn and its contents those are delivered to you on its blogs. :                                                                                                                                                                       Dantino                                20 Feb 2023                                                                                                                      All Stories:             "
+    "body": "      Featured:                                                                                                                                                                                                           Chào mừng đến với trang blog của dantopia. vn                              :               Lời chào Xin chào, tôi là Tống Vũ Thân Dân, dantopia. vn là trang web cá nhân và trang web này là nơi tôi chia sẻ. . . :                                                                                                                                                                       Tống V. T. Dân                                20 Feb 2023                                                                                                                      All Stories:             "
     }, {
     "id": 5,
     "url": "http://localhost:4000/robots.txt",
@@ -32,8 +32,8 @@ var documents = [{
     }, {
     "id": 6,
     "url": "http://localhost:4000/welcome-dantopia-blog/",
-    "title": "Welcome to dantopia.vn",
-    "body": "2023/02/20 - Welcome to dantopia. vn and its contents those are delivered to you on its blogs. Thank you and have a greate start from today! "
+    "title": "Chào mừng đến với trang blog của dantopia.vn",
+    "body": "2023/02/20 - Lời chào: Xin chào, tôi là Tống Vũ Thân Dân, dantopia. vn là trang web cá nhân và trang web này là nơi tôi chia sẻ các kinh nghiệm, kiếm thức,… xoay quanh các chủ đề:    Đổi mới sáng tạo - Innovation     Phát triển sản phẩm (sản phẩm vật lý, hữu hình) - Hardware Product Development     Phát triển doanh nghiệp nhỏ  dantopia - Định nghĩa:: Theo định nghĩa tại Urban Dictionary, dantopia mang ý nghĩa:  A state of happiness, confidence, and inner peace. Often used to describe the emotion felt after some important life event. Tiếng Việt:  Một trạng thái hạnh phúc, tự tin và bình an nội tâm. Thường được dùng để diễn tả cảm xúc sau một số sự kiện quan trọng trong đời. Với hơn 15 năm kinh nghiệm trong các lĩnh vực công nghệ, đặc biệt phát triển sản phẩm sử dụng công nghệ như vi điện tử, lập trình, vi mạch, phát triển phần mềm, Dân sẽ ghi lại những kinh nghiệm với mong muốn chia sẻ những thất bại (nhiều hơn), và thành công trên chặng đường phát triển sự nghiệp của mình. Chúc mọi người thành công, phát đạt , bình an và hạnh phúc: "
     }];
 
 var idx = lunr(function () {
